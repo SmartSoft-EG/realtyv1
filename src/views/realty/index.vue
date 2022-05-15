3dbe726810cd7079a26f4cc4767d43a266181f25
DESKTOP-M17K8P0\smart <abumalek.fayed@gmail.com>
2022-05-15 22:21:26 +0200

    test

--- src/views/realty/index.vue
+++ src/views/realty/index.vue
@@ -71,26 +71,11 @@ function deleteRealty(id: number) {
 
 </script>
 
-<template lang="pug">
-d-page(@refresh="str.fetchRealty()")
-    template(#tools)
-        v-btn(color="primary" @click="addRealty")
-            | Add realty
-    q-table(title="Realty" :filter="search" flat :rows="str.realty_list" :columns="columns" row-key="id")
-        template(#top-right)
-            q-input(v-model="search" outlined dense type="text")
-                template(#append)
-                    q-icon(name="search")
-        template(#body-cell-options="props")
-            td.flex.gap-2
-                v-btn(icon="mdi-eye" variant="outlined" density="compact" color="success"  :to="'/realty/' + props.row.id")
-                v-btn(icon="mdi-pencil" variant="outlined" density="compact" color="primary"  @click="editRealty(props.row)")
-                v-btn(icon="mdi-close" variant="outlined" density="compact" color="error"  @click="deleteRealty(props.row.id)")
+<template>
+<d-page @refresh="str.fetchRealty()"><template #tools><v-btn color="primary" @click="addRealty">Add realty</v-btn></template>
 
-    d-dialog(v-model="str.show_add_dialog" title="Add realty" @save="saveRealty()")
-        q-input(v-model="realty.name" type="text" label="name")
-        q-input(v-model="realty.code" type="text" label="code")
-        q-input(v-model="realty.address" type="text" label="address")
-        q-input(v-model="realty.description" type="textarea" label="description")
-        q-file(v-model="imgs"  multiple label="images")
+<q-table title="Realty" :filter="search" flat :rows="str.realty_list" :columns="columns" row-key="id"><template #top-right><q-input v-model="search" outlined dense type="text"><template #append><q-icon name="search"></q-icon></template></q-input></template><template #body-cell-options="props"><td class="flex gap-2"><v-btn icon="mdi-eye" variant="outlined" density="compact" color="success" :to="'/realty/' + props.row.id"></v-btn><v-btn icon="mdi-pencil" variant="outlined" density="compact" color="primary" @click="editRealty(props.row)"></v-btn><v-btn icon="mdi-close" variant="outlined" density="compact" color="error" @click="deleteRealty(props.row.id)"></v-btn></td></template></q-table>
+
+
+<d-dialog v-model="str.show_add_dialog" title="Add realty" @save="saveRealty()"><q-input v-model="realty.name" type="text" label="name"></q-input><q-input v-model="realty.code" type="text" label="code"></q-input><q-input v-model="realty.address" type="text" label="address"></q-input><q-input v-model="realty.description" type="textarea" label="description"></q-input><q-file v-model="imgs" multiple label="images"></q-file></d-dialog></d-page>
 </template>
