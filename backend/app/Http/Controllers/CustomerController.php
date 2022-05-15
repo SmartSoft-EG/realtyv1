7cbfd42eeebdd13b8c14b53baad3bbb72956185b
DESKTOP-M17K8P0\smart <abumalek.fayed@gmail.com>
2022-05-15 21:34:07 +0200

    add create account during save account

--- backend/app/Http/Controllers/CustomerController.php
+++ backend/app/Http/Controllers/CustomerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 
@@ -10,17 +11,25 @@ class CustomerController extends Controller
     //
     public function index()
     {
-        return Customer::orderBy('updated_at', 'desc')->get();
+        return Customer::orderBy('updated_at', 'desc')->with('account:id,debit,credit,accountable_type,accountable_id')->get();
     }
 
     public function store()
     {
-        return Customer::create(request(['name', 'address', 'email', 'telephone', 'job', 'national_id']));
+        $customer = Customer::create(request(['name', 'address', 'email', 'telephone', 'job', 'national_id']));
+
+        //add customer account
+        //get max record in customers
+        $max = Account::where('parent_id', 1127)->max('id');
+        if (!$max) $max = 112701;
+        else  $max++;
+        $customer->account()->create(['id' => $max, 'parent_id' => 1127, 'name' => 'customer: ' . $customer->name, 'is_main' => 0, 'debit_or_credit' => 'credit']);
+        return $customer->load('account');
     }
 
     public function show(Customer $customer)
     {
-        return $customer;
+        return $customer->load('account');
     }
 
     public function update(Customer $customer)
@@ -28,12 +37,13 @@ class CustomerController extends Controller
 
         $customer->update(request(['name', 'address', 'email', 'telephone', 'job', 'national_id']));
 
-        return response()->json(['success' => 'realty was updated'], 202);
+        return response()->json(['success' => 'customer was updated'], 202);
     }
 
     public function destroy(Customer $customer)
     {
+        $customer->account()->delete();
         $customer->delete();
-        return response()->json(['success' => 'realty was deleted'], 202);
+        return response()->json(['success' => 'customer was deleted'], 202);
     }
 }
